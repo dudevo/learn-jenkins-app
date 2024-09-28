4e3930ca2511e897c0466ca26374081f43ebeb69
dudevo <57369062+dudevo@users.noreply.github.com>
2024-09-28 15:39:55 +0000

    chnage jenkinsfile 2

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -1,4 +1,4 @@
-/*
+
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
@@ -8,7 +8,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
-
+/*
 test('has Jenkins in the body', async ({ page }) => {
   await page.goto('/');
 
